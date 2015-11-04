1d343e45916a8f9cbb773986d37e642ea8116792
Alexander <alexander.zybin@gmail.com>
2015-11-04 15:33:55 +0300

    201511041533/work: It is works!

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,14 @@ var gulp = require('gulp'),
 		reload = browserSync.reload,
 	concat = require('gulp-concat'),					// склейка файлов
 	less = require('gulp-less'),						// LESS
+	//	watchLess = require('gulp-watch-less'),				
 	minifyCss = require('gulp-minify-css'),				// минификация css
 	//myth = require('gulp-myth'),						// префиксы для css - по умолчанию не установлен
 	rename = require('gulp-rename'),					// переименование файлов
-	rigger = require('gulp-rigger'),					// инклуд html
+	//rigger = require('gulp-rigger'),					// инклуд html
 	uglify = require('gulp-uglify'),					// сжатие js
-	watch = require('gulp-watch');						// наблюдение за изменением файловой системы
-
+	watch = require('gulp-watch'),						// наблюдение за изменением файловой системы
+	pagebuilder = require('gulp-pagebuilder');			// умный инклуд html с поддержкой вложенности и передачей параметров
 
 var root = 'site',
 	src = root + '/' + 'src';
@@ -49,27 +50,58 @@ gulp.task('server', function(){
 		server : path.build.root,
 	});
 	
-	gulp.watch(path.src.html + '/*.html', ['build:html']);
-	gulp.watch(path.block.root + '/**/*.less', ['build:less']);
+	gulp.watch(path.src.html + '/**/*.html', ['build:html']);
+	
+	//gulp.watch(path.build.css + '/**/*.less', ['build:less']);
+	//gulp.watch(path.block.root + '/**/*.less', ['build:less']);
+	
+	gulp.watch(path.src.js + '/**/*.js', ['build:js']);
+	gulp.watch(path.block.root + '/**/.*.js', ['build:js']);
+	
+	gulp.watch(path.build.css + '/**/*.less', ['build:css']);
 });
 
-gulp.task('build', ['build:html', 'build:less', ]);
+gulp.task('build', ['build:html', 'build:js', 'build:css' ]);
 
 gulp.task('build:html', function(){
-	//params.pages.forEach(function(item){
-		gulp.src(path.src.html + '/*.html')
-			.pipe(rigger())
-			//.pipe(rename(item))
-			.pipe(gulp.dest(path.build.html))
-			.pipe(reload({stream : true,}))
-		;
-	//});
+	gulp.src(path.src.html + '/**/*.html')
+		//.pipe(rigger())
+		.pipe(pagebuilder(path.build.root))
+		.pipe(gulp.dest(path.build.html))
+		.pipe(reload({stream : true,}))
+	;
 });
 
+gulp.task('build:js', function(){
+	gulp.src(path.src.js + '/**/*.js')
+		//.pipe(rigger())
+		.pipe(pagebuilder(path.build.root))
+		.pipe(uglify())
+		.pipe(gulp.dest(path.build.js))
+		.pipe(reload({stream : true,}))
+	;
+});
+
+gulp.task('build:css', function(){
+	gulp.src(path.build.css + '/site.less')
+		.pipe(less())
+		.pipe(autoprefixer({
+			browsers: ['last 6 versions'],
+			cascade: true,
+		}))
+		.pipe(minifyCss())
+		.pipe(gulp.dest(path.build.css))
+		.pipe(reload({stream : true,}))
+	;
+});
+
+/*
 gulp.task('build:less', function(){
-	gulp.src(path.block.root + '/**/*.less')
+	gulp.src([path.block.root + ])
+		.pipe(watchLess('gulp-import.less'))
 		.pipe(concat('gulp-import.less'))
 		.pipe(gulp.dest(path.build.css + '/site'))
 		.pipe(reload({stream : true,}))
 	;
 });
+*/
---
+++ site/src/js/js.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	
+	[snp tpl="block/fecss/js/document-ready/.page-loader.js" ]
+	
+	[snp tpl="block/fecss/js/document-ready/.scrollto.js" ]
+	
+	[snp tpl="block/fecss/js/document-ready/.arrow-slider.js" ]
+	
+	[snp tpl="block/fecss/js/document-ready/.can-close.js" ]
+	
+});
