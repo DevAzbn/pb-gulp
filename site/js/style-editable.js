72995e3c5f66c81d9525d8a72be9dabfcad23485
Alexander <alexander.zybin@gmail.com>
2016-01-14 15:30:08 +0300

    201601141529/work: Modify style-editable.js

--- site/js/style-editable.js
+++ site/js/style-editable.js
@@ -4,8 +4,8 @@ var style = $('<style/>',{});
 var pre = $('<textarea/>',{});
 
 pre.css({
-		'display':'block',
-		'white-space':'pre',
+		'display':'none',
+		//'white-space':'pre',
 		'position':'fixed',
 		'left':'0',
 		'bottom':'0',
@@ -14,7 +14,7 @@ pre.css({
 		'background-color':'white',
 		'color':'black',
 		'font-size':'13px',
-		'overflow':'auto',
+		//'overflow':'auto',
 		'padding':'5px 30px',
 	})
 	//.attr('contentEditable',true)
@@ -25,7 +25,8 @@ pre.appendTo($('body'));
 
 $(document.body).bind('keydown.fecss', function(event){
 	
-	if(event.ctrlKey && event.which==90) { // Ctrl + Z
+	
+	if(event.ctrlKey && !event.shiftKey && event.which==90) { // Ctrl + Z
 		event.stopPropagation();
 		event.preventDefault();
 		
@@ -35,9 +36,25 @@ $(document.body).bind('keydown.fecss', function(event){
 		
 	}
 	
+	if(event.ctrlKey && event.shiftKey && event.which==90) { // Ctrl + Shift + Z
+		event.stopPropagation();
+		event.preventDefault();
+		
+		pre.toggle();
+		if(pre.is(':visible')) {
+			LS.set('style-editable-visible', 1);
+		} else {
+			LS.set('style-editable-visible', 0);
+		}
+	}
+	
 });
 
 pre.val(LS.get('style-editable'));
 style.html(LS.get('style-editable'));
+
+if(LS.get('style-editable-visible') == 1) {
+	pre.show();
+}
 
 });
